4edb28a308c4c0a1ec586753e6547915af938123
Daniel <Danielbl2010@gmail.com>
2023-02-08 23:06:57 +0200

    add delete route for status and transition

--- routes/basicRoutes.py
+++ routes/basicRoutes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect
-from algorithms.update import update_orphans, update_init
+from algorithms.update import update_orphans, update_init, update_finals
 from classes.Transition import Transition
 from classes.Status import Status
 
@@ -11,7 +11,6 @@ db = setup.db
 
 @basic_routes_handling.route("/", methods=['GET'])
 def get_status():
-
     # packs data as: (Status.name, is_init, is_orphan, is_final)
     status_table = []
 
@@ -27,12 +26,21 @@ def get_status():
 
         if transition_table:
             update_orphans(database=db, status_table=status_table,
-                           init_status_name=init_status_name)
+                           init_status_name=init_status_name, global_init=init_status_name)
 
-        # @todo: write update_final
+            update_finals(database=db, status_table=status_table,
+                          init_status_name=init_status_name, global_init=init_status_name)
 
     return render_template("homepage.html", status_table=status_table, transition_table=transition_table,
                            status_name=request.args.get("statusName", default=""))
+
+
+# @todo: radio input is not working
+@basic_routes_handling.route("/init", methods=['post'])
+def set_init_status():
+    status_table = []
+
+    update_init(status_table=status_table)
 
 
 @basic_routes_handling.route("/status", methods=['post'])
@@ -47,13 +55,21 @@ def add_status():
     return redirect(f'/?statusName={request.form["status_name"]}')
 
 
-@basic_routes_handling.route("/status", methods=['DELETE'])
+@basic_routes_handling.route("/status/delete", methods=['post'])
 def delete_status():
-    # check for duplications
-    status = db.session.query(Status.name).filter_by(name=request.form['status_name']).first()
+    # delete status
+    status = Status.query.filter_by(name=request.form['status_name']).first()
     if status is not None:
-        db.session.remove(status)
+        db.session.delete(status)
         db.session.commit()
+
+        # delete all related transitions
+        transition_list = Transition.query.filter_by(from_status=request.form['status_name']).all()
+        transition_list += Transition.query.filter_by(to_status=request.form['status_name']).all()
+
+        for transition in transition_list:
+            db.session.delete(transition)
+            db.session.commit()
 
         return redirect('/')
     return redirect('/')
@@ -63,8 +79,8 @@ def delete_status():
 def add_transition():
     # checks for duplications in all fields
     if db.session.query(Transition.name).filter_by(name=request.form['name']).first() is None:
-        if db.session.query(Transition).filter(Transition.from_status == request.form['from_status']).first() is None\
-                or db.session.query(Transition).filter(Transition.to_status == request.form['to_status']).first()\
+        if db.session.query(Transition).filter(Transition.from_status == request.form['from_status']).first() is None \
+                or db.session.query(Transition).filter(Transition.to_status == request.form['to_status']).first() \
                 is None:
             # create new status
             if request.form['name'] is not None:
@@ -76,12 +92,12 @@ def add_transition():
     return redirect('/')
 
 
-@basic_routes_handling.route("/transition", methods=['DELETE'])
+@basic_routes_handling.route("/transition/delete", methods=['post'])
 def delete_transition():
     # check for duplications
-    transition = db.session.query(Transition.name).filter_by(name=request.form['status_name']).first()
+    transition = Transition.query.filter_by(name=request.form['transition_name']).first()
     if transition is not None:
-        db.session.remove(transition)
+        db.session.delete(transition)
         db.session.commit()
 
         return redirect('/')
